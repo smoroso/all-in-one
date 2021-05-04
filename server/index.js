1719dae7abc58bafde43f7f54115e2a13420171a
Stephane Moroso <stephane.moroso@gmail.com>
2021-05-04 17:45:38 +0200

    Feat: Improves dynamic view + Layout

--- server/index.js
+++ server/index.js
@@ -9,25 +9,28 @@ import _ from "lodash";
 const startServer = () => {
   const server = express();
   const projectPath = process.cwd();
-  
+
   server.use(compression())	
   server.use(express.static("public")); // necessary to be able to access js and css files on client side
-  
+
   server.get("/", (req, res) => {
     res.sendFile(path.join(projectPath + "/public/index.html"));
   });
 
   server.get("/:name", async (req, res) => {
-    res.send(render(req.params.name, { 'users': ['fred', 'barney'], render }))
+    if(!['about'].includes(req.params.name)) { return res.sendStatus(404); }
+
+    const data = { users: ['fred', 'barney'] };
+    res.send(render('layout', { name: req.params.name, data, render }));
   });
-  
+
   function render(viewName, ctx = {}) {
     const viewPath = path.join(projectPath + `/public/${viewName}.html`);
     if(!fs.existsSync(viewPath)) { return 404; }
 
-    return _.template(fs.readFileSync(viewPath))(ctx)
+    return _.template(fs.readFileSync(viewPath))(ctx);
   }
-  
+
   const listener = server.listen(3000, () => {
     console.log(`Server started, please go on port ${listener.address().port}`);
   });

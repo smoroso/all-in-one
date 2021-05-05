f8f358bbf07f9489f1d11b59e264daa452dbc091
Stephane Moroso <stephane.moroso@gmail.com>
2021-05-05 19:27:29 +0200

    Feat: Moves views outside public and into a views folder + add css to layout

--- server/index.js
+++ server/index.js
@@ -11,21 +11,21 @@ const startServer = () => {
   const projectPath = process.cwd();
 
   server.use(compression())	
-  server.use(express.static("public")); // necessary to be able to access js and css files on client side
+  server.use(express.static(path.join(projectPath, "public"))); // necessary to be able to access js and css files on client side
 
   server.get("/", (req, res) => {
-    res.sendFile(path.join(projectPath + "/public/index.html"));
+    res.sendFile(path.join(projectPath, "/views/index.html"));
   });
 
   server.get("/:name", async (req, res) => {
     if(!['about'].includes(req.params.name)) { return res.sendStatus(404); }
 
     const data = { users: ['fred', 'barney'] };
-    res.send(render('layout', { name: req.params.name, data, render }));
+    res.send(render('layout/layout', { name: req.params.name, data, render }));
   });
 
   function render(viewName, ctx = {}) {
-    const viewPath = path.join(projectPath + `/public/${viewName}.html`);
+    const viewPath = path.join(projectPath + `/views/${viewName}.html`);
     if(!fs.existsSync(viewPath)) { return 404; }
 
     return _.template(fs.readFileSync(viewPath))(ctx);
